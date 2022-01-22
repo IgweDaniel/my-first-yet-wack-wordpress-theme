e41aad26b787f27280ce4ea62c3b988e32b7333e
IgweDaniel <danielchibuzoigwe@gmail.com>
2022-01-22 12:08:04 +0100

    trying to fix comment bug: i hate php

--- BEA/comments.php
+++ BEA/comments.php
@@ -22,23 +22,8 @@
           
             
           </div>
-          <?php
+
           
-          comment_form(
-              array(
-                "class_container"=>"comment__respond",
-                "class_form"=>"comment__form",
-                'author' => '<p class="comment-form-author" data-scroll>' . '<label for="author">' . __( 'Name' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
-                '<input id="author" name="author" type="text" placeholder="Name..." size="30" aria-required="true" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></p>',
-                'email'=> '<p class="comment-form-email"><label for="email">' . __( 'Email' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
-                '<input id="email" name="email" type="text" placeholder="you@email.com" size="30" aria-required="true" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' /></p>',
-              ),
-            );
-
-
-
-           ?>
-
         
 
 </div>
