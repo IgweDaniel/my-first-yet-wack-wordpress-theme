909dbd7529a60ab0d823445181645af26a8fd711
IgweDaniel <danielchibuzoigwe@gmail.com>
2022-01-29 19:22:08 +0100

    showing excerpt for post

--- BEA/template-parts/content-article.php
+++ BEA/template-parts/content-article.php
@@ -24,7 +24,7 @@
          
             <div data-scroll>    
                 <?php
-                    the_content()
+                    the_excerpt()
                 ?>
             </div>
       
